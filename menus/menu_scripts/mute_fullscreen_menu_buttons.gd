2e032717c7252f6f70289938da4c6c789039ff24
Boden McHale <bodenkaikarden@gmail.com>
2024-10-19 08:20:22 -0400

    Fixed global references to fix double click bug

--- menus/menu_scripts/mute_fullscreen_menu_buttons.gd
+++ menus/menu_scripts/mute_fullscreen_menu_buttons.gd
@@ -6,14 +6,14 @@ var is_fullscreen = false
 var master_bus = AudioServer.get_bus_index("Master")
 
 func _on_mute_button_toggled(_toggled_on):
-	if is_game_muted == false:
-		is_game_muted = true
+	if MuteFullscreenMenuButtons.is_game_muted == false:
+		MuteFullscreenMenuButtons.is_game_muted = true
 		AudioServer.set_bus_mute(master_bus, is_game_muted)
 		$MuteButton.texture_normal = preload("res://assets/ui_buttons/muted_button.png")
 		$MuteButton.texture_pressed = preload("res://assets/ui_buttons/muted_button_pressed.png")
 		$MuteButton.texture_hover = preload("res://assets/ui_buttons/muted_button_highlighted.png")
 	else:
-		is_game_muted = false
+		MuteFullscreenMenuButtons.is_game_muted = false
 		AudioServer.set_bus_mute(master_bus, is_game_muted)
 		$MuteButton.texture_normal = preload("res://assets/ui_buttons/not_muted_button.png")
 		$MuteButton.texture_pressed = preload("res://assets/ui_buttons/not_muted_button_pressed.png")
@@ -21,9 +21,9 @@ func _on_mute_button_toggled(_toggled_on):
 	
 
 func _on_fullscreen_button_toggled(_toggled_on):
-	if is_fullscreen == false:
-		is_fullscreen = true
+	if MuteFullscreenMenuButtons.is_fullscreen == false:
+		MuteFullscreenMenuButtons.is_fullscreen = true
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		is_fullscreen = false
+		MuteFullscreenMenuButtons.is_fullscreen = false
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
