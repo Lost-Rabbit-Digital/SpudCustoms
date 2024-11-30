5149474b36edb8a2993a694287f8561b3beef891
David McHale <35319750+dmchaledev@users.noreply.github.com>
2024-11-29 20:32:25 -0700

    Changing potato rain to single potato type, more potato, diagonal potato travel

--- PotatoRain.gd
+++ PotatoRain.gd
@@ -7,17 +7,11 @@ extends Node2D
 func _ready():
 	# Create animated texture with multiple frames
 	var animated_texture = AnimatedTexture.new()
-	animated_texture.frames = 4  # Set to number of potato textures you have
+	animated_texture.frames = 1  # Set to number of potato textures you have
 	
 	# Load your potato texture
 	var potato_texture = preload("res://assets/potatoes/bodies/yukon_gold_body.png")
-	var potato_texture_1 = preload("res://assets/potatoes/bodies/russet_burbank_body.png")
-	var potato_texture_2 = preload("res://assets/potatoes/bodies/sweet_potato_body.png")
-	var potato_texture_3 = preload("res://assets/potatoes/bodies/purple_majesty_body.png")
 	animated_texture.set_frame_texture(0, potato_texture)
-	animated_texture.set_frame_texture(1, potato_texture_1)
-	animated_texture.set_frame_texture(2, potato_texture_2)
-	animated_texture.set_frame_texture(3, potato_texture_3)
 	# Assign to particle system
 	particle_system.texture = animated_texture
 	
@@ -32,7 +26,7 @@ func setup_particle_system():
 	process_material.emission_box_extents = Vector3(viewport_size.x / 2, 1, 1)
 	
 	# Particle behavior
-	process_material.direction = Vector3(0, 1, 0)
+	process_material.direction = Vector3(0.25, 1, 0)
 	process_material.spread = 10.0
 	process_material.gravity = Vector3(0, 98, 0)
 	process_material.initial_velocity_min = 50.0
@@ -54,8 +48,8 @@ func setup_particle_system():
 	particle_system.process_material = process_material
 	
 	# Configure particle system node
-	particle_system.amount = 50
+	particle_system.amount = 100
 	particle_system.lifetime = 4.0
-	particle_system.explosiveness = 0.0
+	particle_system.explosiveness = 0.1
 	particle_system.randomness = 1.0
 	particle_system.visibility_rect = Rect2(-viewport_size.x/2, -50, viewport_size.x, viewport_size.y + 100)
