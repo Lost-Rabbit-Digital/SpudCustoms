e952466df3dbbf33c3d5a7969fe72c914b598612
Boden McHale <bodenkaikarden@gmail.com>
2025-03-15 20:46:53 -0400

    Trying to fix z-order issue with passport

--- godot_project/scenes/game_scene/mainGame.gd
+++ godot_project/scenes/game_scene/mainGame.gd
@@ -454,7 +454,7 @@ func animate_mugshot_and_passport():
 	tween.tween_property(passport, "modulate:a", 1, 1)
 	tween.tween_property(passport, "visible", true, 0).set_delay(1)
 	tween.tween_property(passport, "position:y", passport_spawn_point_end.position.y, 0.7).set_delay(1)
-	tween.tween_property(passport, "z_index", 5, 0).set_delay(3)
+	tween.tween_property(passport, "z_index", 9, 0).set_delay(3)
 
 	tween.chain().tween_callback(func(): print("Finished animating mugshot and passport"))
 	
