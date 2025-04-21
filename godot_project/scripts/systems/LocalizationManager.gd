b02ebaeee44b6033f13bbe9cb1dd0c25fc22be6e
David McHale <35319750+dmchaledev@users.noreply.github.com>
2025-04-21 16:41:59 -0600

    Updating translations for Spud Customs

--- godot_project/scripts/systems/LocalizationManager.gd
+++ godot_project/scripts/systems/LocalizationManager.gd
@@ -4,11 +4,9 @@ extends Node
 signal language_changed(language_code)
 
 var available_languages = {
-	"bg": "български",       # Bulgarian
 	"cs": "Čeština",         # Czech
 	"da": "Dansk",           # Danish
 	"de": "Deutsch",         # German
-	"el": "Ελληνικά",        # Greek
 	"en": "English",         # English
 	"es": "Español",         # Spanish (Spain)
 	"fi": "Suomi",           # Finnish
@@ -16,31 +14,26 @@ var available_languages = {
 	"hu": "Magyar",          # Hungarian
 	"id": "Bahasa Indonesia", # Indonesian
 	"it": "Italiano",        # Italian
-	"ja": "日本語",           # Japanese - Test font support
-	"ko": "한국어",           # Korean - Test font support
 	"nl": "Nederlands",      # Dutch
 	"no": "Norsk",           # Norwegian
 	"pl": "Polski",          # Polish
 	"pt": "Português",       # Portuguese (Portugal)
 	"ro": "Română",          # Romanian
-	"ru": "Русский",         # Russian
 	"sk": "Slovenčina",      # Slovak
 	"sv": "Svenska",         # Swedish
-	"th": "ไทย",             # Thai - Test font support
 	"tr": "Türkçe",          # Turkish
-	"uk": "Українська",      # Ukrainian
 	"vi": "Tiếng Việt",      # Vietnamese
-	"zh-CN": "简体中文",      # Chinese (Simplified) - Test font support
 }
 
-var current_language = "es"
+var current_language = "en"
 
 func _ready():
 	# Set initial language (maybe from a saved setting)
-	var saved_lang = Config.get_config("GameSettings", "Language", "es")
+	var saved_lang = Config.get_config("GameSettings", "Language", "en")
 	#set_language(saved_lang)
-	set_language("ko")
-
+	set_language("vi")
+	TranslationServer.set_locale("vi")
+	
 func set_language(language_code):
 	if language_code in available_languages:
 		TranslationServer.set_locale(language_code)
