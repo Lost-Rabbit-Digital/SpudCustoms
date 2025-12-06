971a77a759721ca8b9903c5f164ce751dd95c110
David McHale <35319750+dmchaledev@users.noreply.github.com>
2025-12-06 15:52:23 -0700

    Removing duplicate dialogic, moving sentry to correct addons folder

--- addons/dialogic/Modules/LayeredPortrait/custom_layered_portrait.gd
+++
@@ -1,2 +0,0 @@
-@tool
-extends "res://addons/dialogic/Modules/LayeredPortrait/layered_portrait.gd"
--- addons/dialogic/Modules/StyleEditor/index.gd
+++
@@ -1,6 +0,0 @@
-@tool
-extends DialogicIndexer
-
-
-func _get_editors() -> Array:
-	return [this_folder.path_join("style_editor.tscn")]
